2dbb343c1685027d9caacbb624a4fd74810c7b51
pglazkov <glazprom@mail.ru>
2025-01-14 21:19:15 +0300

    add readers

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to the Weather API")
-	name := flag.String("name", "Имя по умолчанию", "Имя пользователя")
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
 
 	flag.Parse()
 
-	fmt.Printf("Hello, %s!\n", *name)
+	fmt.Printf("Город, %s!\n", *city)
+	fmt.Println(*format)
 
+	r := strings.NewReader("Привет! я поток данных!")
+	block := make([]byte, 4)
+	for {
+		_, err := r.Read(block)
+		fmt.Printf("%q\n", block)
+		if err == io.EOF {
+			break
+		}
+	}
 }
